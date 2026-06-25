e504d6e7c44a35ed20733fdf5d458b3640694ceb
Rajani <patil.rajani.1990@gmail.com>
2026-06-25 16:19:06 +0530

    :Pushing arraylist program

---
+++ ArraylistExample1.java
@@ -0,0 +1,14 @@
+package collectionpkg;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ArraylistExample1 {
+
+	public static void main(String[] args) 
+	{
+		
+		ChromeDriver driver=new ChromeDriver();
+		
+	}
+
+}
